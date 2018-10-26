d4bcd588de24903a5d653be9cfd8dfe7f971d075
Milen Halachev <milenski88@gmail.com>
2018-10-26 11:01:23 +0300

    Added xcode project file

---
+++ HeartlandSDK/HeartlandSDK.h
@@ -0,0 +1,19 @@
+//
+//  HeartlandSDK.h
+//  HeartlandSDK
+//
+//  Created by Milen Halachev on 26.10.18.
+//  Copyright © 2018 Heartland Payment Systems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HeartlandSDK.
+FOUNDATION_EXPORT double HeartlandSDKVersionNumber;
+
+//! Project version string for HeartlandSDK.
+FOUNDATION_EXPORT const unsigned char HeartlandSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HeartlandSDK/PublicHeader.h>
+
+
