80ba4b27dd9abacac65854ee53da21d6551a3be2
Wilson Desimini <wdesimini8@gmail.com>
2021-04-26 15:25:17 -0700

    add sample controller for gms device for HRPOS

---
+++ Example/Heartland-iOS-SDK/HRGMSDeviceViewController.m
@@ -0,0 +1,22 @@
+//
+//  HRGMSDeviceViewController.m
+//  Heartland-iOS-SDK_Example
+//
+//  Created by Desimini, Wilson on 4/26/21.
+//  Copyright © 2021 Shaunti Fondrisi. All rights reserved.
+//
+
+#import "HRGMSDeviceViewController.h"
+
+@interface HRGMSDeviceViewController ()
+
+@end
+
+@implementation HRGMSDeviceViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+@end
