373281e6a73e1f28ab3f8882dbe20d9ba58cb66b
Shane Logsdon <shane@shanelogsdon.com>
2021-06-18 13:02:07 -0400

    allow timeout to be set on GMS

--- Pod/Classes/Terminals/Utility/GlobalMobileSDK/GMSConfiguration.swift
+++ Pod/Classes/Terminals/Utility/GlobalMobileSDK/GMSConfiguration.swift
@@ -23,6 +23,9 @@ public class GMSConfiguration: NSObject {
             config.developerId = c.developerID
             config.versionNumber = c.versionNumber
             config.environment = c.isProduction ? .production : .certification
+            if let timeout = c.timeout {
+                config.timeout = Int32(timeout.pointee)
+            }
         }
         config.merchantName = ""
         config.merchantNumber = ""
