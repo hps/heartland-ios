6c0d458ef267900b8e9c4a10eaf18e68b8ecafea
Wilson Desimini <wdesimini8@gmail.com>
2022-07-01 09:33:30 -0700

    HR-9575 add UPA data parsing utility

---
+++ Pod/Classes/Terminals/UPA/Interfaces/HpaUPAParser.h
@@ -0,0 +1,21 @@
+//
+//  HpaUPAParser.h
+//  Heartland-iOS-SDK
+//
+//  Created by Desimini, Wilson on 7/1/22.
+//
+
+#import <Foundation/Foundation.h>
+#import "UpaEnums.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HpaUPAParser : NSObject
+
++ (NSData * _Nullable)dataFromUPARaw:(NSData *)data;
++ (NSDictionary * _Nullable)jsonfromUPARaw:(NSData *)data;
++ (UPA_MSG_TYPE)upaMessageTypeFromUPARaw:(NSString *)message;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Pod/Classes/Terminals/UPA/Interfaces/HpaUPAParser.m
@@ -0,0 +1,51 @@
+//
+//  HpaUPAParser.m
+//  Heartland-iOS-SDK
+//
+//  Created by Desimini, Wilson on 7/1/22.
+//
+
+#import "HpaUPAParser.h"
+#import "HpsTerminalEnums.h"
+
+@implementation HpaUPAParser
+
++ (NSData *)dataFromUPARaw:(NSData *)data {
+    NSString *rawString = [[NSString alloc] initWithData:data
+                                                encoding:NSASCIIStringEncoding];
+    NSString *separator = [NSString stringWithFormat:
+                           @"%c%c%c%c%c",
+                           (char) HpsControlCodes_LF,
+                           (char) HpsControlCodes_ETX,
+                           (char) HpsControlCodes_LF,
+                           (char) HpsControlCodes_STX,
+                           (char) HpsControlCodes_LF];
+    if ([rawString containsString:separator]) {
+        NSArray *parts = [rawString componentsSeparatedByString:separator];
+        rawString = parts[1];
+    }
+    NSCharacterSet *separatorCharSet = [NSCharacterSet characterSetWithCharactersInString:separator];
+    rawString = [rawString stringByTrimmingCharactersInSet:separatorCharSet];
+    return [rawString dataUsingEncoding:NSUTF8StringEncoding];
+}
+
++ (NSDictionary *)jsonfromUPARaw:(NSData *)data {
+    NSData *dataParsed = [self dataFromUPARaw:data];
+    if (dataParsed == nil) return nil;
+    return [NSJSONSerialization JSONObjectWithData:dataParsed options:0 error:nil];
+}
+
++ (UPA_MSG_TYPE)upaMessageTypeFromUPARaw:(NSString *)message {
+    NSDictionary *upaMessageTypesByRaw = @{
+        @"ACK": @(UPA_MSG_TYPE_ACK),
+        @"NAK": @(UPA_MSG_TYPE_NAK),
+        @"READY": @(UPA_MSG_TYPE_READY),
+        @"BUSY": @(UPA_MSG_TYPE_BUSY),
+        @"TO": @(UPA_MSG_TYPE_TIMEOUT),
+        @"MSG": @(UPA_MSG_TYPE_MSG),
+        @"DATA": @(UPA_MSG_TYPE_DATA), };
+    NSNumber *typePointer = upaMessageTypesByRaw[message];
+    return typePointer ? [typePointer longValue] : UPA_MSG_TYPE_UNKNOWN;
+}
+
+@end
