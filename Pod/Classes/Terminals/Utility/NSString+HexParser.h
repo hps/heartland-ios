44bcf496d2529fd56896adf31ec08a5ae98f80c4
Wilson Desimini <wdesimini8@gmail.com>
2022-10-05 13:18:42 -0700

    HR-11671 convert hexadecimal strings

---
+++ Pod/Classes/Terminals/Utility/NSString+HexParser.h
@@ -0,0 +1,14 @@
+//
+//  NSString+HexParser.h
+//  Heartland-iOS-SDK
+//
+//  Created by Desimini, Wilson on 10/5/22.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (HexParser)
+
++ (instancetype)stringWithHex:(NSString *)hex;
+
+@end
